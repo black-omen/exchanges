80f2d0f4b3897c1f53cb88596ba57f84677f0b6e
gagdiez <gagdiez.c@gmail.com>
2017-05-19 23:41:39 +0200

    Add abc for exchanges
    This commit adds an abstract base class for Exchanges. It requires
    that subclasses implement methods to get the server time, get the
    latest trade, place orders, obtain information about orders, and
    get trades history.

---
+++ base.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+
+
+class Exchange(ABC):
+    """This class defines the minimum set of methods to interact
+        with an Exchanges"""
+
+    @abstractmethod
+    def server_time(self):
+        """Queries the current server time"""
+        pass
+
+    @abstractmethod
+    def latest_trade(self, asset_pair):
+        """Retrieves the latest trade made on the supplied asset"""
+        pass
+
+    @abstractmethod
+    def place_order(self, order):
+        """Places an order in the exchange"""
+        pass
+
+    @abstractmethod
+    def order_info(self, order_id):
+        """Retrieves information about an order"""
+        pass
+
+    @abstractmethod
+    def trades_history(self, asset_pair, start_time, finish_time):
+        """Retrieves the trades made between the supplied time interval"""
+        pass
